887338c3ee37507af7cdee0e4baeefaae2b11d4a
Dipu <103568666+immdipu@users.noreply.github.com>
2022-04-23 15:26:33 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 const line01 = document.querySelector('.line1');
 const line02 = document.querySelector('.line2');
 const line03 = document.querySelector('.line3');
+const overLay = document.querySelector('.overlay');
+const hamburger = document.querySelector('.hamburger-container');
 const hiddenhamburgurnavbar = document.querySelector('.hamburgurnavbar');
 
-document.querySelector('.hamburger-container').addEventListener('click', function(){
+hamburger.addEventListener('click', function(){
 line01.classList.toggle('firstline');
 line02.classList.toggle('secondline');
 line03.classList.toggle('thirdline');
+overLay.classList.toggle('hidden');
 hiddenhamburgurnavbar.classList.toggle('hamburgurnavbarjava');
 
 })
+
+overLay.addEventListener('click', function(){
+line01.classList.toggle('firstline');
+line02.classList.toggle('secondline');
+line03.classList.toggle('thirdline');
+overLay.classList.toggle('hidden');
+hiddenhamburgurnavbar.classList.toggle('hamburgurnavbarjava');
+    
+});
